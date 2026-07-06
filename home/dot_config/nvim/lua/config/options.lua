13571a2e73c15730d2a414f5194746df9a19f6c9
yesitsfebreeze <sayhello@hvlmnns.de>
2026-07-06 10:20:21 +0200

    intermediate

--- home/dot_config/nvim/lua/config/options.lua
+++ home/dot_config/nvim/lua/config/options.lua
@@ -19,9 +19,9 @@ opt.cmdheight = 1
 opt.pumheight = 10
 
 opt.expandtab = true
-opt.shiftwidth = 4
-opt.tabstop = 4
-opt.softtabstop = 4
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.softtabstop = 2
 opt.smartindent = true
 opt.breakindent = true
 
